2714c843618895e36a17d559f3b733e95becbd8a
Bini Babu <bini.babu@dbizsolution.com>
2024-09-27 10:57:06 +0530

    Primitive Data types example

---
+++ src/com/bini/babu/DataTypes.java
@@ -0,0 +1,15 @@
+package com.bini.babu;
+
+public class DataTypes {
+    byte a = -50;
+    short b = 150;
+    int c = 10000;
+    long d = 100000;
+    float e = 1.23F;
+    double f = 1234.56789;
+    char h = 'A';
+    byte g = 20;
+    
+    int result = a+g;
+
+}
